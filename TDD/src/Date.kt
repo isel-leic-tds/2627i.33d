02c32ec12369034c78074ac95e46b9e7e5e83f2a
Pedro Pereira <593033+palex65@users.noreply.github.com>
2026-09-23 21:17:54 +0100

    Aula 23Set (Date V1..V4)

--- TDD/src/Date.kt
+++ TDD/src/Date.kt
@@ -1,4 +1,11 @@
-class Date(val year: Int, val month: Int = 1, val day: Int = 1)
+
+class Date(val year: Int, val month: Int = 1, val day: Int = 1) {
+    init {
+        require(year in 1582..2200)
+        require(month in 1..12) { "Month must be between 1 and 12" }
+        require(day in 1..lastDayOfMonth) { "Day must be between 1 and $lastDayOfMonth" }
+    }
+}
 
 val Int.isLeapYear: Boolean
     get() = this % 4 == 0 && this % 100 != 0 || this % 400 == 0
---
+++ TDD/src/main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    println("Hello, TDD!")
+    val date = Date(2026, 15, 30)
+    println("Date created = $date")
+    println(date.lastDayOfMonth)
+}
